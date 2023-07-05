06acae2b40bf83f55f0d5ef14b79ea22b288f43b
jogg15 <jogger.cztechnic@gmail.com>
2023-07-05 20:10:16 +0200

    1.0.0

--- src/main/java/dev/digitality/velocitycommands/commands/VelocityCommands.java
+++ src/main/java/dev/digitality/velocitycommands/commands/VelocityCommands.java
@@ -4,6 +4,8 @@ import com.velocitypowered.api.command.SimpleCommand;
 import dev.digitality.velocitycommands.managers.ConfigManager;
 import dev.digitality.velocitycommands.utils.ChatUtils;
 
+import java.util.List;
+
 
 public class VelocityCommands implements SimpleCommand {
     @Override
@@ -20,46 +22,54 @@ public class VelocityCommands implements SimpleCommand {
         if (invocation.arguments()[0].equals("overriden")) {
             if (invocation.arguments()[1].equals("add")) {
                 if (invocation.arguments().length == 4) {
-                    ConfigManager.getConfig().set(invocation.arguments()[2], invocation.arguments()[3]);
-                    ConfigManager.saveConfig(ConfigManager.getConfig(), "settings.yml");
-                    invocation.source().sendMessage(ChatUtils.getPrefix().append(ChatUtils.colorize("<green>Added overriden command <gray>/" + invocation.arguments()[2])));
+                    if (ConfigManager.getConfig().getStringList("overriden").contains(invocation.arguments()[2])) {
+                        ConfigManager.getConfig().set("overriden." + invocation.arguments()[2], invocation.arguments()[3]);
+                        ConfigManager.saveConfig(ConfigManager.getConfig(), "settings.yml");
+                        invocation.source().sendMessage(ChatUtils.getPrefix().append(ChatUtils.colorize("<green>Added overriden command <gray>/" + invocation.arguments()[2])));
+                    } else {
+                        invocation.source().sendMessage(ChatUtils.getPrefix().append(ChatUtils.colorize("<white>Usage: <aqua>/vcmds <overriden/disabled> <add/remove> <command>")));
+                    }
+                } else if (invocation.arguments()[1].equals("remove")) {
+                    if (invocation.arguments().length == 3) {
+                        ConfigManager.getConfig().set(invocation.arguments()[2], null);
+                        ConfigManager.saveConfig(ConfigManager.getConfig(), "settings.yml");
+                        invocation.source().sendMessage(ChatUtils.getPrefix().append(ChatUtils.colorize("<red>Removed overriden command <gray>/" + invocation.arguments()[2])));
+                    } else {
+                        invocation.source().sendMessage(ChatUtils.getPrefix().append(ChatUtils.colorize("<white>Usage: <aqua>/vcmds <overriden/disabled> <add/remove> <command>")));
+                    }
                 } else {
                     invocation.source().sendMessage(ChatUtils.getPrefix().append(ChatUtils.colorize("<white>Usage: <aqua>/vcmds <overriden/disabled> <add/remove> <command>")));
                 }
-            } else if (invocation.arguments()[1].equals("remove")) {
-                if (invocation.arguments().length == 3) {
-                    ConfigManager.getConfig().set(invocation.arguments()[2], null);
-                    ConfigManager.saveConfig(ConfigManager.getConfig(), "settings.yml");
-                    invocation.source().sendMessage(ChatUtils.getPrefix().append(ChatUtils.colorize("<red>Removed overriden command <gray>/" + invocation.arguments()[2])));
+            } else if (invocation.arguments()[0].equals("disabled")) {
+                if (invocation.arguments()[1].equals("add")) {
+                    if (invocation.arguments().length == 3) {
+                        List<String> disabledCommands = ConfigManager.getConfig().getStringList("disabled-commands");
+                        disabledCommands.add(invocation.arguments()[2]);
+                        ConfigManager.getConfig().set("disabled-commands", disabledCommands);
+                        ConfigManager.saveConfig(ConfigManager.getConfig(), "settings.yml");
+                        invocation.source().sendMessage(ChatUtils.getPrefix().append(ChatUtils.colorize("<green>Added disabled command <gray>/" + invocation.arguments()[2])));
+                    } else {
+                        invocation.source().sendMessage(ChatUtils.getPrefix().append(ChatUtils.colorize("<white>Usage: <aqua>/vcmds <overriden/disabled> <add/remove> <command>")));
+                    }
+                } else if (invocation.arguments()[1].equals("remove")) {
+                    if (invocation.arguments().length == 3) {
+                        List<String> disabledCommands = ConfigManager.getConfig().getStringList("disabled-commands");
+                        disabledCommands.remove(invocation.arguments()[2]);
+                        ConfigManager.getConfig().set("disabled-commands", disabledCommands);
+                        ConfigManager.saveConfig(ConfigManager.getConfig(), "settings.yml");
+                        invocation.source().sendMessage(ChatUtils.getPrefix().append(ChatUtils.colorize("<red>Removed disabled command <gray>/" + invocation.arguments()[2])));
+                    } else {
+                        invocation.source().sendMessage(ChatUtils.getPrefix().append(ChatUtils.colorize("<white>Usage: <aqua>/vcmds <overriden/disabled> <add/remove> <command>")));
+                    }
                 } else {
                     invocation.source().sendMessage(ChatUtils.getPrefix().append(ChatUtils.colorize("<white>Usage: <aqua>/vcmds <overriden/disabled> <add/remove> <command>")));
                 }
             } else {
                 invocation.source().sendMessage(ChatUtils.getPrefix().append(ChatUtils.colorize("<white>Usage: <aqua>/vcmds <overriden/disabled> <add/remove> <command>")));
             }
-        } else if (invocation.arguments()[0].equals("disabled")) {
-            if (invocation.arguments()[1].equals("add")) {
-                if (invocation.arguments().length == 3) {
-                    ConfigManager.getConfig().set("disabled-commands", ConfigManager.getConfig().getStringList("disabled-commands").add(invocation.arguments()[2]));
-                    ConfigManager.saveConfig(ConfigManager.getConfig(), "settings.yml");
-                    invocation.source().sendMessage(ChatUtils.getPrefix().append(ChatUtils.colorize("<green>Added disabled command <gray>/" + invocation.arguments()[2])));
-                } else {
-                    invocation.source().sendMessage(ChatUtils.getPrefix().append(ChatUtils.colorize("<white>Usage: <aqua>/vcmds <overriden/disabled> <add/remove> <command>")));
-                }
-            } else if (invocation.arguments()[1].equals("remove")) {
-                if (invocation.arguments().length == 3) {
-                    ConfigManager.getConfig().set("disabled-commands", ConfigManager.getConfig().getStringList("disabled-commands").remove(invocation.arguments()[2]));
-                    ConfigManager.saveConfig(ConfigManager.getConfig(), "settings.yml");
-                    invocation.source().sendMessage(ChatUtils.getPrefix().append(ChatUtils.colorize("<red>Removed disabled command <gray>/" + invocation.arguments()[2])));
-                } else {
-                    invocation.source().sendMessage(ChatUtils.getPrefix().append(ChatUtils.colorize("<white>Usage: <aqua>/vcmds <overriden/disabled> <add/remove> <command>")));
-                }
-            } else {
-                invocation.source().sendMessage(ChatUtils.getPrefix().append(ChatUtils.colorize("<white>Usage: <aqua>/vcmds <overriden/disabled> <add/remove> <command>")));
-            }
-        } else {
-            invocation.source().sendMessage(ChatUtils.getPrefix().append(ChatUtils.colorize("<white>Usage: <aqua>/vcmds <overriden/disabled> <add/remove> <command>")));
+
         }
 
     }
 }
+
